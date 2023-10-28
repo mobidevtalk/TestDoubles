1e79abba6ac60237268b7a34510f1f8c9e379f34
shadmazumder <shadmazumder@gmail.com>
2023-10-28 09:20:58 +0600

    Initiating Encoding fail test

---
+++ UnitTestFundamentalTests/EncodingFailTests.swift
@@ -0,0 +1,12 @@
+//
+//  EncodingFailTests.swift
+//  UnitTestFundamentalTests
+//
+//  Created by Shad Mazumder on 28/10/23.
+//
+
+import XCTest
+
+final class EncodingFailTests: XCTestCase {
+    
+}
