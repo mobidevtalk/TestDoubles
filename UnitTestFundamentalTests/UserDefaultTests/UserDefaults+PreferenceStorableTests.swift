f98eb912e37ee7c21a30f0ebd30c95f036735751
shadmazumder <shadmazumder@gmail.com>
2023-10-28 12:50:29 +0600

    Moving SUT creation into a helper method

--- UnitTestFundamentalTests/UserDefaultTests/UserDefaults+PreferenceStorableTests.swift
+++ UnitTestFundamentalTests/UserDefaultTests/UserDefaults+PreferenceStorableTests.swift
@@ -10,29 +10,34 @@ import UnitTestFundamental
 
 final class UserDefaults_PreferenceStorableTests: XCTestCase {
     func test_save_withEmptyData_returnEmptyDataError() {
-        let sut = UserDefaults(suiteName: "UserDefaultsTestSuite")
+        let sut = makeSUT()
         
-        let error = sut?.save(Data(), for: "anyKey") as? UserDefaultsError
+        let error = sut.save(Data(), for: "anyKey") as? UserDefaultsError
         
         XCTAssertEqual(error, .emptyData)
     }
     
     func test_save_withEmptyKey_returnEmptyKeyError() {
-        let sut = UserDefaults(suiteName: "UserDefaultsTestSuite")
+        let sut = makeSUT()
         
-        let error = sut?.save("some-data".data(using: .utf8)!, for: "") as? UserDefaultsError
+        let error = sut.save("some-data".data(using: .utf8)!, for: "") as? UserDefaultsError
         
         XCTAssertEqual(error, .emptyKey)
     }
     
     func test_save_withValidDataAndKey_storesData() {
         let key = "any-onther-key"
-        let sut = UserDefaults(suiteName: "UserDefaultsTestSuite")
+        let sut = makeSUT(key)
         
-        let error = sut?.save("some-data".data(using: .utf8)!, for: key)
+        let error = sut.save("some-data".data(using: .utf8)!, for: key)
         
-        let data = sut?.data(forKey: key)
+        let data = sut.data(forKey: key)
         XCTAssertNil(error)
         XCTAssertNotNil(data)
+    }
+    
+    // MARK: - Helper
+    private func makeSUT(_ key: String = "UserDefaultsTestSuite") -> UserDefaults{
+        UserDefaults(suiteName: key)!
     }
 }
