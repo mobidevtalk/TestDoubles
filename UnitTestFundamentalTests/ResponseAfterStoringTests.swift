1cc1f37af18cc98b147101a5853e580c3f601483
shadmazumder <shadmazumder@gmail.com>
2023-10-28 08:41:31 +0600

    Adding response test
    - We will verify the response after each storing

---
+++ UnitTestFundamentalTests/ResponseAfterStoringTests.swift
@@ -0,0 +1,11 @@
+//
+//  ResponseAfterStoringTests.swift
+//  UnitTestFundamentalTests
+//
+//  Created by Shad Mazumder on 28/10/23.
+//
+
+import XCTest
+
+final class ResponseAfterStoringTests: XCTestCase {
+}
