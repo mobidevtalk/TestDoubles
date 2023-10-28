259e15a6bc6469a8ce9cd78b3046aaae02b108b9
shadmazumder <shadmazumder@gmail.com>
2023-10-28 08:25:31 +0600

    Confirming save preference stores the data
    - Inititing models
    - Confirming Encodable to encode them on storing time

--- UnitTestFundamentalTests/TestDoubles.swift
+++ UnitTestFundamentalTests/TestDoubles.swift
@@ -7,12 +7,37 @@
 
 import XCTest
 
+struct Credential {
+    let username: String
+    let password: String
+}
+
+extension Credential: Encodable{}
+
+enum Preference {
+    case notNow
+    case never
+    case save(Credential)
+}
+
+extension Preference: Encodable{}
+
 protocol PreferenceStorable{
     func save(_ data: Data, for key: String) -> Error?
 }
 
 struct StorageController {
     let presistenceStore: PreferenceStorable
+    
+    func save(preference: Preference){
+        let encoder = JSONEncoder()
+        guard let data = try? encoder.encode(preference) else { return }
+        _ = presistenceStore.save(data, for: Self.PreferenceStoreKey)
+    }
+}
+
+extension StorageController{
+    static var PreferenceStoreKey: String {"PreferenceStoreKey"}
 }
 
 class TestDoublesTests: XCTestCase {
@@ -20,6 +45,15 @@ class TestDoublesTests: XCTestCase {
         let (_, spy) = makeSUT()
         
         XCTAssertTrue(spy.preferences.isEmpty)
+    }
+    
+    func test_savePreference_storesPref() {
+        let (sut, spy) = makeSUT()
+        XCTAssertTrue(spy.preferences.isEmpty)
+        
+        sut.save(preference: .never)
+        
+        XCTAssertFalse(spy.preferences.isEmpty)
     }
     
     // MARK: - Helper
