6a30c0b39be294f7bfd4ef3f41c75f1e3d815277
shadmazumder <shadmazumder@gmail.com>
2023-10-28 08:02:57 +0600

    Dropping to make main clean

--- UnitTestFundamentalTests/TestDoubles.swift
+++ UnitTestFundamentalTests/TestDoubles.swift
@@ -4,37 +4,3 @@
 //
 //  Created by Shad Mazumder on 22/10/23.
 //
-
-import XCTest
-
-protocol PreferenceStorable{
-    func save(_ data: Data, for key: String) -> Error?
-}
-
-struct StorageController {
-    let presistenceStore: PreferenceStorable
-}
-
-class TestDoublesTests: XCTestCase {
-    func test_init_doesnotStorePreference() {
-        let (_, spy) = makeSUT()
-        
-        XCTAssertTrue(spy.preferences.isEmpty)
-    }
-    
-    // MARK: - Helper
-    private func makeSUT() -> (sut: StorageController, spy: SpyPreferenceStorage) {
-        let spy = SpyPreferenceStorage()
-        let sut = StorageController(presistenceStore: spy)
-        return (sut, spy)
-    }
-}
-
-class SpyPreferenceStorage: PreferenceStorable {
-    private(set) var preferences = [String: Data]()
-    
-    func save(_ data: Data, for key: String) -> Error? {
-        preferences[key] = data
-        return nil
-    }
-}
